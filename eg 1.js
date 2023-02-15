8054debb522ba3cf88bb7a6b660cdf25713c597e
Paulo Amaral <53989502+phfa26@users.noreply.github.com>
2023-02-15 17:45:51 +1100

    Update eg 1.js
    Review

--- eg 1.js
+++ eg 1.js
@@ -2,3 +2,6 @@ function myFunction() {
   alert("Hello World!");
 }
 myFunction();
+
+//Almost there, but you are criating an Alert, and the exercise asked for a console prompt.
+//Also, theres no need to put it in a function, a single line with alert("Hello World!"); would do the same job. Just nit-picking, btw.
