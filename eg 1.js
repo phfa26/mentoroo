fce6cc329dd691ce774d2900f90a412673f22034
Lucas Miletti Costa <115209911+lucasmiletti@users.noreply.github.com>
2023-02-15 20:07:19 +1100

    Update eg 1.js

--- eg 1.js
+++ eg 1.js
@@ -5,3 +5,5 @@ myFunction();
 
 //Almost there, but you are criating an Alert, and the exercise asked for a console prompt.
 //Also, theres no need to put it in a function, a single line with alert("Hello World!"); would do the same job. Just nit-picking, btw.
+
+console.log("Hello world!");
