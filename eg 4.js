14ce1cadc8ba3cb577ab3cdada9d2cb53d48b0b4
Paulo Amaral <53989502+phfa26@users.noreply.github.com>
2023-02-15 18:00:08 +1100

    Update eg 4.js

--- eg 4.js
+++ eg 4.js
@@ -6,3 +6,15 @@ function myFunction(a, b) {
   }
 }
 myFunction(9, 5);
+
+//Good work. 
+//Later, in a couple of weeks when you gain more experience, re-write this using an arrow function
+//Other ideas for you to think about:
+
+// function myFunction(a, b) {
+//    a >= b ? console.log(a) : console.log(b)
+// };
+
+// function myFunction(a, b) {
+//    console.log(Math.max(a,b));
+// };
