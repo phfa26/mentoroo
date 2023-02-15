d8874f9c0384553f9e5822398a19667ee8d73eab
Paulo Amaral <53989502+phfa26@users.noreply.github.com>
2023-02-15 18:06:16 +1100

    Update eg 7.js

--- eg 7.js
+++ eg 7.js
@@ -5,3 +5,10 @@ function reverseString(str) {
   return joinArr;
 }
 console.log(reverseString("Eusouolucas"));
+//Well done! Pro tip: do not use 'let' if you are not mutating that value at a later stage. Try 'const' instead.
+//Also, further down the track consider chaining. You can use array methods in a chain, like: 
+
+//function reverseString(str) {
+//  const joinArr = str.split("").reverse().join("");
+//  return joinArr;
+//}
